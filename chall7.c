22099ec9f5af207177f873c7a33f830e5db36916
Vigneswar-A <93377157+Vigneswar-A@users.noreply.github.com>
2026-02-04 15:17:38 +0530

    Create chall7.c

---
+++ chall7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char *flag = "flag{n0_w4y_c4nt_y0u_s33_m3}";
+    char s[16];
+    printf("Enter your name: ");
+    fgets(s, 15, stdin);
+    printf("Hello, ");
+    printf(s);
+    printf("Nice to meet you!\n");
+    return 0;
+}
