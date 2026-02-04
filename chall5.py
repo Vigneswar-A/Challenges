5051f7f985a9cf6989b3858c4c71622ea1456e5f
Vigneswar-A <93377157+Vigneswar-A@users.noreply.github.com>
2026-02-04 12:43:25 +0530

    Create chall5.py

---
+++ chall5.py
@@ -0,0 +1,3 @@
+from string import ascii_letters as letters
+flag = "flag{Y0u_c4n_3v4l_n1ce}"
+eval(expr if (len(expr := input("Enter a math expression: ")) <= 6 and all(c.lower() not in letters for c in expr)) else 'print("Can\'t compute that much!")')
